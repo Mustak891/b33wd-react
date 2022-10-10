b1a8dccdc5837c0f00f94c8ac4684e1de6458af3
FIRST_NAME LAST_NAME <mustakahammed891@gmail.com>
2022-10-10 13:19:46 +0530

    good

--- src/App.js
+++ src/App.js
@@ -1,52 +1,28 @@
-import "./App.css"; 
+import "./App.css";
 
-export default function Appbox(){
-     const numbers = [{
-       name : "FREE",
-       packages: "$0",
-       singleuser: "✔ Single User",
-       storage: "✔ 5GB Storage",
-       projects: "✔ Unlimited Public Projects",
-       access: "✔ Community Access",
-       privates: "✖ Unlimited Private Projects",
-       dedicated: "✖ Dedicated Phone Support",
-       subdomain:"✖ Free Subdomain",
-       monthly :"✖ Monthly Status Reports"
-     },{
-      name : "PLUS",
-      packages: "$9",
-      singleuser: "✔ 5 User",
-      storage: "✔ 50GB Storage",
-      projects: "✔ Unlimited Public Projects",
-      access: "✔ Community Access",
-      privates: "✔ Unlimited Private Projects",
-      dedicated: "✔ Dedicated Phone Support",
-      subdomain:"✔ Free Subdomain",
-      monthly :"✖ Monthly Status Reports"
-     },{
-      name : "PRO",
-      packages: "$49",
-      singleuser: "✔ Unlimited Users",
-      storage: "✔ 150GB Storage",
-      projects: "✔ Unlimited Public Projects",
-      access: "✔ Community Access",
-      privates: "✔ Unlimited Private Projects",
-      dedicated: "✔ Dedicated Phone Support",
-      subdomain:"✔ Free Subdomain",
-      monthly :"✔ Monthly Status Reports"
-     }]
-     return(
-       <div className="box_1" >
-         {numbers.map((number) => (
-         <Detail name={number.name} packages={number.packages} singleuser={number.singleuser} storage={number.storage} projects={number.projects} access={number.access} 
-         privates={number.privates} dedicated={number.dedicated} subdomain={number.subdomain} monthly={number.monthly} />
-         ))}
-       </div>
-     )
+export default function Appbox() {
+
+  return (
+    <div className="box_1" >
+       <form>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required />
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required />
+
+    <button type="submit">Login</button>
+  </div>
+
+</form>
+    </div>
+  )
 }
 
-function Detail({name, packages, singleuser, storage, projects, access, privates, dedicated, subdomain, monthly}){
-  return(
+function Detail({ name, packages, singleuser, storage, projects, access, privates, dedicated, subdomain, monthly }) {
+  return (
     <div className="numbox">
       <div>
         <h3 className="nametop">{name}</h3>
@@ -55,7 +31,7 @@ function Detail({name, packages, singleuser, storage, projects, access, privates
         <h1 className="namebottom">{packages}<span className="month">/month</span></h1>
       </div>
       <div>
-        <h3> {singleuser}</h3>
+        <h3> {singleuser}</h3>x
       </div>
       <div>
         <h3>{storage}</h3>
@@ -79,7 +55,7 @@ function Detail({name, packages, singleuser, storage, projects, access, privates
         <h3>{monthly}</h3>
       </div>
       <div>
-      <button className="button">Button</button>
+        <button className="button">Button</button>
       </div>
     </div>
   )
